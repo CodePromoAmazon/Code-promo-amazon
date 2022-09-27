9fc7a7ba59472cc37b26f7cc2e67fa92357ee711
CodePromoAmazon <114483988+CodePromoAmazon@users.noreply.github.com>
2022-09-27 13:07:01 +0200

    Update index.js

--- pages/index.js
+++ pages/index.js
@@ -22,8 +22,8 @@ export default function Home() {
           La solution pour faire de grandes économies quand on achète sur Amazon
          </p>
        
-        <p className="description">Des centaines de <a href="https://www.les-codes-promos.com/code-promo-amazon/" target="_blank">growth hacks</a> enrichis CHAQUE JOUR</p>
-        <p className="description">Près de 100 <a href="https://www.les-codes-promos.com/code-promo-amazon/">ressources pour entrepreneurs</a> téléchargeables</p>
+        <p className="description">Codes promo Amazon 2022 <a href="https://www.les-codes-promos.com/code-promo-amazon/" target="_blank">growth hacks</a> code promo 5€ Amazon</p>
+        <p className="description">Près de 100 <a href="https://www.les-codes-promos.com/code-promo-amazon/">codes promotionnels</a> codes promotionnels </p>
         <p className="description">
           Toutes les réductions et promotions <a href="https://www.les-codes-promos.com/code-promo-amazon/">Promos Flash</a>, de <a href="https://www.les-codes-promos.com/code-promo-amazon">Prime day</a>, Livraison en Point retrait <a href="https://www.les-codes-promos.com/code-promo-amazon">Livraison Gratuite</a>, Soldes Amazon
         </p>
